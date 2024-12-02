1dd6654c2c10193de180230a95832c4f6f18dbfd
Linnea <148188744+NEAAA4@users.noreply.github.com>
2024-12-02 11:11:02 +0100

    Update linked_list.h

--- linked_list.h
+++ linked_list.h
@@ -1,36 +1,30 @@
+// linked_list.h
 #ifndef LINKED_LIST_H
 #define LINKED_LIST_H
 
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <stdbool.h>
+#include "memory_manager.h" // Include your custom memory manager
 #include <stdint.h>
-#include <stddef.h>
 #include <pthread.h>
+typedef struct Node
+{
+    uint16_t data;     // Stores the data as an unsigned 16-bit integer
+    struct Node *next; // Pointer to the next node in the list
+    pthread_mutex_t lock;
 
-#define pool_size 1024 // storlek på memory pool
-#define block_size 16 // storlek på memory block
+} Node;
 
-extern char memory_pool[pool_size]; 
-extern bool allocated[pool_size/block_size];
-extern size_t num_blocks;
+// Function declarations
+void list_init(Node **head, size_t size);
+void list_insert(Node **head, uint16_t data);
+void list_insert_after(Node *prev_node, uint16_t data);
+void list_insert_before(Node **head, Node *next_node, uint16_t data);
+void list_delete(Node **head, uint16_t data);
+Node *list_search(Node **head, uint16_t data);
 
-typedef struct Node {
-    uint16_t data;
-    struct Node* next;
-} Node; 
+void list_display(Node **head);
+void list_display_range(Node **head, Node *start_node, Node *end_node);
 
-
-void list_init(Node** head, size_t size);
-void list_insert(Node** head, uint16_t data);
-void list_insert_after(Node* prev_node, uint16_t data);
-void list_insert_before(Node** head, Node* next_node, uint16_t data);
-void list_delete(Node** head, uint16_t data);
-Node* list_search(Node** head, uint16_t data);
-void list_display(Node** head);
-void list_display_range(Node** head, Node* start_node, Node* end_node);
-int list_count_nodes(Node** head);
-void list_cleanup(Node** head);
+int list_count_nodes(Node **head);
+void list_cleanup(Node **head);
 
 #endif // LINKED_LIST_H
