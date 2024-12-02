646301e107f93ddaefb18e424e4432df8869273b
Linnea <148188744+NEAAA4@users.noreply.github.com>
2024-12-02 11:10:48 +0100

    Update linked_list.c

--- linked_list.c
+++ linked_list.c
@@ -1,247 +1,221 @@
-// lico22
-
-#include "memory_manager.h"
-#include "common_defs.h"
-
+#include "linked_list.h"
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
-#include <stdbool.h>
-#include <stdint.h>
-#include <stddef.h>
+#include <string.h>
 #include <pthread.h>
 
-#define pool_size 1024
-#define block_size 16
-
-char memory_pool[pool_size];
-bool allocated[pool_size/block_size];
-size_t num_blocks;
-
-
-typedef struct Node {
-    uint16_t data;
-    struct Node* next;
-} Node; 
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 
 void list_init(Node** head, size_t size) {
+    pthread_mutex_lock(&lock);
     *head = NULL;
-    Node* node = NULL;
-
-    for (size_t i = 0; i < size; i++) {
-        Node* node = (Node*)mem_alloc(sizeof(Node));
-        if (node == NULL) {
-            return; // failed tomt
-        }
-    }
-    node->next = *head;
-    *head = node;
+    mem_init(size); 
+    pthread_mutex_unlock(&lock);
 }
 
 void list_insert(Node** head, uint16_t data) {
-    pthread_mutex_t mem_lock; // skapar en mutex
-    pthread_mutex_init(&mem_lock, NULL); 
-    pthread_mutex_lock(&mem_lock); // låser med mutex
-
-    Node* node = (Node*)mem_alloc(sizeof(Node));
-    if (node == NULL) {
-        printf("Memory allocation failed, no node \n");
-        pthread_mutex_unlock(&mem_lock); // faild unlock
+    pthread_mutex_lock(&lock);
+    Node* new = (Node*) mem_alloc(sizeof(Node));
+    if (new == NULL) {
+        printf("Memory allocation failed\n");
+        pthread_mutex_unlock(&lock);
         return;
     }
 
-    node->data = data;
-    node->next = NULL;
+    new->data = data;
+    new->next = NULL;
 
     if (*head == NULL) {
-        *head = node;
-    }
-    else {
+        *head = new; 
+    } else {
         Node* here = *head;
         while (here->next != NULL) {
             here = here->next;
         }
-        here->next = node;
+        here->next = new;
     }
-    pthread_mutex_unlock(&mem_lock);
+    pthread_mutex_unlock(&lock);
 }
 
 void list_insert_after(Node* prev_node, uint16_t data) {
-    pthread_mutex_t mem_lock; // skapar en mutex
-    pthread_mutex_init(&mem_lock, NULL); 
-    pthread_mutex_lock(&mem_lock); // låser med mutex
-    
-    Node* node = (Node*)mem_alloc(sizeof(Node));
-    if (node == NULL) {
-        printf("Memory allocation failed, no node \n");
-        pthread_mutex_unlock(&mem_lock); // faild unlock
+    if (prev_node == NULL) {
+        printf("Prev_node is NULL\n");
         return;
     }
-    
-    node->data = data;
-    node->next = prev_node->next;
-    prev_node->next = node;
 
-    pthread_mutex_unlock(&mem_lock);
+    pthread_mutex_lock(&lock);
+    Node* new = (Node*) mem_alloc(sizeof(Node));
+    if (new == NULL) {
+        printf("Memory allocation failed\n");
+        pthread_mutex_unlock(&lock);
+        return;
+    }
+
+    new->data = data;
+    new->next = prev_node->next;
+    prev_node->next = new;
+    pthread_mutex_unlock(&lock);
 }
 
-void list_insert_before(Node** head, Node* next_node, uint16_t data){
-    pthread_mutex_t mem_lock; // skapar en mutex
-    pthread_mutex_init(&mem_lock, NULL); 
-    pthread_mutex_lock(&mem_lock); // låser med mutex
+void list_insert_before(Node** head, Node* next_node, uint16_t data) {
+    pthread_mutex_lock(&lock);
+    if (next_node == *head) {
+        Node* new = (Node*) mem_alloc(sizeof(Node));
+        if (new == NULL) {
+            printf("Memory allocation failed\n");
+            pthread_mutex_unlock(&lock);
+            return;
+        }
+        new->data = data;
+        new->next = *head;
+        *head = new;
+    } else {
+        Node* prev_node = *head;
+        while (prev_node != NULL && prev_node->next != next_node) {
+            prev_node = prev_node->next;
+        }
 
-    Node* node = (Node*)mem_alloc(sizeof(Node));
-    if (next_node == NULL) {
-        printf("Memory allocated failed, no node \n");
-        pthread_mutex_unlock(&mem_lock);
-        return;
-    }
-    
-    node->data = data; // node får datan
+        if (prev_node == NULL) {
+            printf("Next_node not in list\n");
+            pthread_mutex_unlock(&lock);
+            return;
+        }
 
-    if (*head == next_node) {
-        node->next = *head; // ifall pekaren för head är på nextnode så flyttas den en steg
-        *head = node;
-        pthread_mutex_unlock(&mem_lock);
-        return;
-    }
+        Node* new = (Node*) mem_alloc(sizeof(Node));
+        if (new == NULL) {
+            printf("Memory allocation failed\n");
+            pthread_mutex_unlock(&lock);
+            return;
+        }
 
-    Node* here = *head;
-    while (here->next != next_node && here->next != NULL) {
-        here = here->next;
+        new->data = data;
+        new->next = next_node;
+        prev_node->next = new;
     }
-
-    if (here->next == next_node) {
-        node->next = next_node; // pekar på nästa
-        here->next = node; // pekaren updateras till nästa
-    }
-    pthread_mutex_unlock(&mem_lock);
+    pthread_mutex_unlock(&lock);
 }
 
 void list_delete(Node** head, uint16_t data) {
-    pthread_mutex_t mem_lock; // skapar en mutex
-    pthread_mutex_init(&mem_lock, NULL); 
-    pthread_mutex_lock(&mem_lock); // låser med mutex
+    pthread_mutex_lock(&lock);
+    if (*head == NULL || head == NULL) {
+        printf("List is empty\n");
+        pthread_mutex_unlock(&lock);
+        return;
+    }
 
-    if (*head == NULL) {
-        pthread_mutex_unlock(&mem_lock);
+    Node* temp = NULL;
+
+    if ((*head)->data == data) {
+        temp = *head;
+        *head = (*head)->next;
+        mem_free(temp);
+        pthread_mutex_unlock(&lock);
         return;
     }
 
     Node* here = *head;
-    Node* bfore = NULL;
-
-    if (here != NULL && here->data == data) {
-        *head = here->next;
-        mem_free(here);
-        pthread_mutex_unlock(&mem_lock);
-        return;
-    }
-
-    while (here->data != data && here != NULL ) {
-        bfore = here;
+    while (here->next != NULL) {
+        if (here->next->data == data) {
+            temp = here->next;
+            here->next = here->next->next;
+            mem_free(temp);
+            pthread_mutex_unlock(&lock);
+            return;
+        }
         here = here->next;
     }
 
-    if (here == NULL) {
-        pthread_mutex_unlock(&mem_lock);
-        return; // finns ej
-    }
-
-    bfore->next = here->next;
-    mem_free(here);
+    printf("Data not found in list\n");
+    pthread_mutex_unlock(&lock);
 }
 
 Node* list_search(Node** head, uint16_t data) {
-    pthread_mutex_t mem_lock; // skapar en mutex
-    pthread_mutex_init(&mem_lock, NULL); 
-    pthread_mutex_lock(&mem_lock); // låser med mutex
+    pthread_mutex_lock(&lock);
+    if (head == NULL || *head == NULL) {
+        pthread_mutex_unlock(&lock);
+        return NULL;
+    }
 
     Node* here = *head;
     while (here != NULL) {
         if (here->data == data) {
-            pthread_mutex_unlock(&mem_lock);
+            pthread_mutex_unlock(&lock);
             return here;
         }
         here = here->next;
     }
+
+    pthread_mutex_unlock(&lock);
     return NULL;
 }
 
 void list_display(Node** head) {
-    Node* here = *head; // sätter pekaren på start värdet
+    pthread_mutex_lock(&lock);
+    Node* here = *head;
     printf("[");
-    while (here != NULL) {
-        printf("%d", here->data);
-        if (here->next != NULL) {
+    while (here) {
+        printf("%u", here->data);
+        if (here->next) {
             printf(", ");
         }
-        here = here->next; // flyttar pekaren ett steg
+        here = here->next;
     }
-    printf("]");
+    printf("]\n");
+    pthread_mutex_unlock(&lock);
 }
 
-void list_display_range(Node** head, Node* start_node, Node* end_node){
-    pthread_mutex_t mem_lock; // skapar en mutex
-    pthread_mutex_init(&mem_lock, NULL); 
-    pthread_mutex_lock(&mem_lock); // låser med mutex
-
-    Node* here = *head;
+void list_display_range(Node** head, Node* start_node, Node* end_node) {
+    pthread_mutex_lock(&lock);
+    if (*head == NULL) {
+        printf("[]");
+        pthread_mutex_unlock(&lock);
+        return;
+    }
 
     if (start_node == NULL) {
         start_node = *head;
     }
 
-    while (here != NULL && here != start_node) {
-        here = here->next;
-    }
+    Node* here = start_node;
+    printf("[");
+    int start = 1;
 
-    if (here == NULL) {
-        printf("[] \n");
-        pthread_mutex_unlock(&mem_lock);
-        return;
-    }
     while (here != NULL) {
-        printf("%d", here->data);
+        if (!start) {
+            printf(", ");
+        }
+        printf("%u", here->data);
         if (here == end_node) {
-            pthread_mutex_unlock(&mem_lock);
             break;
         }
         here = here->next;
+        start = 0;
     }
-
-    printf("\n");
-    pthread_mutex_unlock(&mem_lock);
+    printf("]");
+    pthread_mutex_unlock(&lock);
 }
 
 int list_count_nodes(Node** head) {
-    pthread_mutex_t mem_lock; // skapar en mutex
-    pthread_mutex_init(&mem_lock, NULL); 
-    pthread_mutex_lock(&mem_lock); // låser med mutex
-
-    Node* here = *head;
+    pthread_mutex_lock(&lock);
     int count = 0;
+    Node* here = *head;
     while (here != NULL) {
         count++;
         here = here->next;
     }
-    pthread_mutex_unlock(&mem_lock);
+    pthread_mutex_unlock(&lock);
     return count;
 }
 
 void list_cleanup(Node** head) {
-    pthread_mutex_t mem_lock; // skapar en mutex
-    pthread_mutex_init(&mem_lock, NULL); 
-    pthread_mutex_lock(&mem_lock); // låser med mutex
-    
+    pthread_mutex_lock(&lock);
     Node* here = *head;
-    Node* node;
-
     while (here != NULL) {
-        node = here->next;
+        Node* next = here->next;
         mem_free(here);
-        here = node;
+        here = next;
     }
     *head = NULL;
-    pthread_mutex_unlock(&mem_lock);
+    mem_deinit();
+    pthread_mutex_unlock(&lock);
 }
